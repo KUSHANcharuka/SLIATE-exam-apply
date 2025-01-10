46a2a9758c6f3af515da032488b2dd363631358d
KUSHANcharuka <150827136+KUSHANcharuka@users.noreply.github.com>
2025-01-10 19:58:07 +0530

    update
    department changes to selection

---
+++ login.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login Page</title>
+    <link rel="stylesheet" href="style2.css">
+</head>
+<body>
+
+    <div class="login-container">
+        <h2>Login</h2>
+        <form method="POST" action="register.php">
+            <div class="form-group">
+                <label for="regNumber">Registration Number</label>
+                <input type="text" id="regNumber" name="regNumber" placeholder="Enter Registration Number" required>
+            </div>
+
+            <div class="form-group">
+                <label for="password">Password</label>
+                <input type="password" id="password" name="password" placeholder="Enter Password" required>
+            </div>
+
+            <div class="button-container">
+                <button type="submit">Login</button>
+            </div>
+
+            <p class="register-message">Don't have an account? <a href="signup.php">Register here</a></p>
+        </form>
+    </div>
+
+
+<script src="script.js"></script>
+
+</body>
+</html>
